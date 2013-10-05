f8c18ea539cdf6a7f7bc0ab2e60893e4b74d0aaa
Franco Catena <francocatena@gmail.com>
2013-10-05 12:50:38 -0300

    Clase 4

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Agenda::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'personas#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
